9b0ae13dcf66ee57321e7433aee9584a2fbe7c1c
root <root@localhost.localdomain>
2026-06-25 13:35:42 +0800

    chore: remove .tmp_* temp scripts, add .gitignore rule
    Fixes 8 CodeQL high-severity alerts (clear-text logging of sensitive data).
    All alerts were in temporary helper scripts that should never have been committed.

--- .tmp_redeploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-export VERCEL_TOKEN=$(python3 -c "import json; print(json.load(open('/root/.vercel/auth.json'))['token'])")
-cd /root/Paylabs
-vercel deploy --token="$VERCEL_TOKEN" --yes 2>&1 | tail -5
