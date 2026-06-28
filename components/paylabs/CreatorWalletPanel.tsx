64b81db7a4a61c9a73a058fc47a8c5c700375405
riyan <128919668+riyannode@users.noreply.github.com>
2026-06-28 15:15:56 +0700

    Fix creator Google login gesture flow (#59)

--- components/paylabs/CreatorWalletPanel.tsx
+++ components/paylabs/CreatorWalletPanel.tsx
@@ -110,7 +110,7 @@ export default function CreatorWalletPanel() {
         ucwGoogleError={ucwGoogleError}
         onPrepareGoogleLogin={() => { prepareGoogleLogin().catch(() => {}); }}
         onRetryPrepareGoogleLogin={() => { retryPrepareGoogleLogin().catch(() => {}); }}
-        autoPrepareGoogleLogin={true}
+        autoPrepareGoogleLogin={false}
         showEmailLogin={false}
         onDisconnect={disconnect}
       />
